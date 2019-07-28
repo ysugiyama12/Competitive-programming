6a3af96e453441b94ef38a9a13efaeea16b139c0
ysugiyama12 <yuji9511yuji@gmail.com>
2019-07-28 21:04:02 +0900

    20190728

--- _base.cpp
+++ _base.cpp
@@ -1,16 +1,16 @@
+/*** author: yuji9511 ***/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> lpair;
 const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
 #define rep(i,m,n) for(ll i = (m); i < (n); i++)
 #define rrep(i,m,n) for(ll i = (m); i >= (n); i--)
 #define print(x) cout << (x) << endl;
 #define print2(x,y) cout << (x) << " " << (y) << endl;
-#define printa(x,n) for(ll i = 0; i < n; i++){ cout << (x[i]) << " ";} cout<<endl;
-#define INF (1e18)
+#define printa(x,n) for(ll i = 0; i < n; i++){ cout << (x[i]) << " \n"[i==n-1];};
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    
+	cin.tie(0);
+	ios::sync_with_stdio(false);
 }
